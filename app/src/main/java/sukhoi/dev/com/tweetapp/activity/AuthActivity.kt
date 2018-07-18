173b23b589b7265f3db3ca94305773431946e617
sukhoi27 <sukhoikms26@gmail.com>
2018-07-19 01:16:51 +0300

    add layer with "what's new" feature

--- app/src/main/java/sukhoi/dev/com/tweetapp/activity/AuthActivity.kt
+++ app/src/main/java/sukhoi/dev/com/tweetapp/activity/AuthActivity.kt
@@ -1,7 +1,9 @@
 package sukhoi.dev.com.tweetapp.activity
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import com.twitter.sdk.android.core.Callback
 import com.twitter.sdk.android.core.Result
@@ -9,9 +11,9 @@ import com.twitter.sdk.android.core.identity.TwitterLoginButton
 import sukhoi.dev.com.tweetapp.R
 import com.twitter.sdk.android.core.TwitterException
 import com.twitter.sdk.android.core.TwitterSession
-
-
-
+import io.github.tonnyl.whatsnew.WhatsNew
+import io.github.tonnyl.whatsnew.item.WhatsNewItem
+import io.github.tonnyl.whatsnew.util.PresentationOption
 
 
 class AuthActivity: AppCompatActivity() {
@@ -31,6 +33,27 @@ class AuthActivity: AppCompatActivity() {
             override fun failure(exception: TwitterException) {
             }
         }
+
+        val whatsnew = WhatsNew.newInstance(
+                WhatsNewItem("Find your friend!", "Now you can find your friends and see what they do!", R.drawable.baseline_search_24),
+                WhatsNewItem("Faster!", "Increase speed of loaders", R.drawable.round_fast_forward_24),
+                WhatsNewItem("Better!", "Fix more crashes", R.drawable.baseline_thumb_up_24),
+                WhatsNewItem("Stronger!", "Protect your data", R.drawable.baseline_verified_user_24))
+
+        with(whatsnew) {
+            presentationOption = PresentationOption.DEBUG
+
+            titleColor = ContextCompat.getColor(this@AuthActivity, R.color.colorPrimary)
+            titleText = "What's Up"
+
+            buttonText = "Got it!"
+            buttonBackground = ContextCompat.getColor(this@AuthActivity, R.color.colorPrimary)
+            buttonTextColor = ContextCompat.getColor(this@AuthActivity, R.color.white)
+
+            itemContentColor = Color.parseColor("#808080")
+            itemTitleColor = ContextCompat.getColor(this@AuthActivity, R.color.colorPrimaryDark)
+        }
+        whatsnew.presentAutomatically(this@AuthActivity)
     }
 
     fun navigateToUserInfo(userId: Long) {
